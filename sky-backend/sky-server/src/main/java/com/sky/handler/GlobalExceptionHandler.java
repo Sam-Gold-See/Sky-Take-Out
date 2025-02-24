6e8fad72acb7e339cff56bde4eb6be12cfa4c9a1
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-02-24 16:00:58 +0800

    完善SQL违反约束异常类的异常处理器类

--- sky-backend/sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-backend/sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -15,13 +18,33 @@ public class GlobalExceptionHandler {
 
     /**
      * 捕获业务异常
-     * @param ex
-     * @return
+     *
+     * @param ex BaseException 业务异常类
+     * @return Result响应对象类
      */
     @ExceptionHandler
-    public Result exceptionHandler(BaseException ex){
+    public Result<String> exceptionHandler(BaseException ex) {
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
     }
 
+    /**
+     * 处理SQL违反约束异常
+     *
+     * @param ex SQLIntegrityConstraintViolationException SQL违反约束异常
+     * @return Result响应对象类
+     */
+    @ExceptionHandler
+    public Result<String> exceptionHandler(SQLIntegrityConstraintViolationException ex) {
+        //Duplicate entry 'admin' for key 'employee.idx_username'
+        String message = ex.getMessage();
+        if (message.contains("Duplicate entry")) {
+            String[] split = message.split(" ");
+            String username = split[2];
+            String msg = username + MessageConstant.ALREADY_EXISTS;
+            return Result.error(msg);
+        } else {
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
+    }
 }
