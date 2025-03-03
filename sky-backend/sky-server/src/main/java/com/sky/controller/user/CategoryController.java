045d7016051142c58efdc621c192ee6a6b4e90f2
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-03 09:51:04 +0800

    实现客户端获取分类列表的业务需求

---
+++ sky-backend/sky-server/src/main/java/com/sky/controller/user/CategoryController.java
@@ -0,0 +1,37 @@
+package com.sky.controller.user;
+
+import com.sky.entity.Category;
+import com.sky.result.Result;
+import com.sky.service.CategoryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController("userCategoryController")
+@Slf4j
+@Api("分类相关接口")
+@RequestMapping("/user/category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    /**
+     * 获取分类列表
+     *
+     * @param type 查询分类类型：1 菜品分类 2 套餐分类
+     * @return Result类响应对象
+     */
+    @ApiOperation("获取分类列表")
+    @GetMapping("/list")
+    public Result<List<Category>> getCategoryListPage(Integer type) {
+        List<Category> list = categoryService.getCategoryListByType(type);
+        return Result.success(list);
+    }
+}
