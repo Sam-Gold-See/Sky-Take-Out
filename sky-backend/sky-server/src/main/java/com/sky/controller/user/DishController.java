a7d24ec250b2a7aa6b09ac8da4235eb5cd57cca2
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-04 11:16:58 +0800

    实现了使用Redis缓存菜品数据的业务需求

--- sky-backend/sky-server/src/main/java/com/sky/controller/user/DishController.java
+++ sky-backend/sky-server/src/main/java/com/sky/controller/user/DishController.java
@@ -9,6 +9,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,9 @@ public class DishController {
     @Autowired
     DishService dishService;
 
+    @Autowired
+    RedisTemplate redisTemplate;
+
     /**
      * 根据分类id查询菜品
      *
@@ -33,12 +37,25 @@ public class DishController {
     @ApiOperation("根据分类id查询菜品")
     @GetMapping("/list")
     public Result<List<DishVO>> getDishVOListByCategoryId(Long categoryId) {
+
+        //构造redis中的key，规则：dish_分类id
+        String key = "dish_" + categoryId;
+
+        //查询redis中是否存在菜品数据
+        List<DishVO> list = (List<DishVO>) redisTemplate.opsForValue().get(key);
+        if (list != null && !list.isEmpty())
+            //如果存在，直接返回，无需查询数据库
+            return Result.success(list);
+
         Dish dish = Dish.builder()
                 .categoryId(categoryId)
                 .status(StatusConstant.ENABLE)
                 .build();
 
-        List<DishVO> list = dishService.getDishVOListByCategoryId(dish);
+        list = dishService.getDishVOListByCategoryId(dish);
+
+        //如果不存在，查询数据库，将查询到的数据放入redis
+        redisTemplate.opsForValue().set(key, list);
 
         return Result.success(list);
     }
