90440980d0135ce96b9e758839eba0366f3db9df
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-04 14:42:20 +0800

    实现了使用Redis缓存套餐数据的业务需求

--- sky-backend/sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-backend/sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -10,6 +10,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -44,6 +45,7 @@ public class SetmealController {
      */
     @PostMapping
     @ApiOperation("新增套餐")
+    @CacheEvict(cacheNames = "setmealCache", key = "#setmealDTO.categoryId")
     public Result insertSetmeal(@RequestBody SetmealDTO setmealDTO) {
         setmealService.insertSetmeal(setmealDTO);
         return Result.success();
@@ -71,6 +73,7 @@ public class SetmealController {
      */
     @PostMapping("/status/{status}")
     @ApiOperation("起售/停售套餐")
+    @CacheEvict(cacheNames = "setmealCache", allEntries = true)
     public Result startOrStopSetmeal(@PathVariable Integer status, Long id) {
         setmealService.startOrStopSetmeal(status, id);
         return Result.success();
@@ -84,6 +87,7 @@ public class SetmealController {
      */
     @DeleteMapping
     @ApiOperation("批量删除套餐")
+    @CacheEvict(cacheNames = "setmealCache", allEntries = true)
     public Result deleteSetmealById(@RequestParam List<Long> ids) {
         setmealService.deleteSetmealByIds(ids);
         return Result.success();
@@ -97,6 +101,7 @@ public class SetmealController {
      */
     @PutMapping
     @ApiOperation("修改套餐")
+    @CacheEvict(cacheNames = "setmealCache", allEntries = true)
     public Result updateSetmeal(@RequestBody SetmealDTO setmealDTO) {
         setmealService.updateSetmeal(setmealDTO);
         return Result.success();
