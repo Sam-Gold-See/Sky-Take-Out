9c322c7dafa3c2270635c5efafedb87f4923c77a
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-05 15:59:24 +0800

    导入微信支付相关代码

--- sky-backend/sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-backend/sky-server/src/main/java/com/sky/service/OrderService.java
@@ -1,6 +1,8 @@
 package com.sky.service;
 
+import com.sky.dto.OrdersPaymentDTO;
 import com.sky.dto.OrdersSubmitDTO;
+import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
 
 public interface OrderService {
@@ -12,4 +14,19 @@ public interface OrderService {
      * @return OrderSubmitVO 订单提交VO对象
      */
     OrderSubmitVO submit(OrdersSubmitDTO ordersSubmitDTO);
+
+    /**
+     * 订单支付
+     *
+     * @param ordersPaymentDTO 用户订单支付DTO对象
+     * @return OrderPaymentVO 订单支付VO对象
+     */
+    OrderPaymentVO payment(OrdersPaymentDTO ordersPaymentDTO) throws Exception;
+
+    /**
+     * 支付成功，修改订单状态
+     *
+     * @param outTradeNo
+     */
+    void paySuccess(String outTradeNo);
 }
