e2dd4618e4ee6d8db7632ca710592745d08d703e
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2025-03-06 14:43:25 +0800

    实现订单超时自动取消的业务需求

--- sky-backend/sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-backend/sky-server/src/main/java/com/sky/SkyApplication.java
@@ -4,9 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableScheduling //开启任务调度
 @EnableTransactionManagement //开启注解方式的事务管理
 @EnableCaching //开启注解方式的缓存管理
 @Slf4j
---
+++ sky-backend/sky-server/src/main/java/com/sky/task/OrderTask.java
@@ -0,0 +1,36 @@
+package com.sky.task;
+
+import com.sky.entity.Orders;
+import com.sky.mapper.OrderMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Component
+@Slf4j
+public class OrderTask {
+
+    @Autowired
+    OrderMapper orderMapper;
+
+    /**
+     * 处理超时订单
+     */
+    @Scheduled(cron = "0 * * * * ?")
+    public void processTimeOutOrder() {
+        LocalDateTime time = LocalDateTime.now().plusMinutes(-15);
+        List<Orders> ordersList = orderMapper.getByStatusAndOrderTimeLT(Orders.PENDING_PAYMENT, time);
+
+        if (ordersList != null && !ordersList.isEmpty())
+            for (Orders order : ordersList) {
+                order.setStatus(Orders.CANCELLED);
+                order.setCancelReason("订单超时，自动取消");
+                order.setCancelTime(LocalDateTime.now());
+                orderMapper.update(order);
+            }
+    }
+}
